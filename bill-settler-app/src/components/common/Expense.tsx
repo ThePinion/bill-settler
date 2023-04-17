e0666abdee0a3dfb38dd68ce5fdf694d1b5d628d
Jakub Kowalczyk <jakub8.kowalczyk@student.uj.edu.pl>
2023-04-17 11:13:10 +0200

    Expense component improved

--- bill-settler-app/src/components/common/Expense.tsx
+++ bill-settler-app/src/components/common/Expense.tsx
@@ -5,26 +5,36 @@ export class Expense {
     public name: string;
     public owner: string;
     public cost: number;
+    public settled: boolean;
 
-    constructor(name: string, owner: string, cost: number) {
+    constructor(name: string, owner: string, cost: number, settled: boolean) {
         this.name = name;
         this.owner = owner;
         this.cost = cost;
+        this.settled = settled;
     }
 
 }
 
 const ExpenseComponent: Component<{expense: Expense}> = (props) => {
+
+    let settledString = props.expense.settled ? "Settled" : "Unsettled"
+
     return(
         <button 
-            class="flex flex-row flex-wrap rounded-xl p-4 m-2 w-96 shadow-sm
+            class="flex flex-row flex-wrap items-center justify-center rounded-xl px-4 py-2 m-2 shadow-sm
             bg-gray-700 hover:bg-[#253041] transition-all duration-200 hover:rounded-lg"
         >
-            <div class="font-semibold flex-auto">{props.expense.name}</div>
-            <Gap/>
-            <div class="font-light">{"paid by " + props.expense.owner}</div>
-            <Gap/>
-            <div class="w-24 text-right">{props.expense.cost + "$"}</div>
+            <div class="flex flex-col text-left w-52 overflow-hidden">
+                <div class="font-semibold text-lg">{props.expense.name}</div>
+                <div class="font-extralight text-sm">Paid by {props.expense.owner}</div>
+            </div>
+            <div class="w-16 text-right font-mono font-semibold text-green-500 text-opacity-80">
+                {props.expense.cost}$
+            </div>
+            <div class="w-16 ml-4 text-right text-gray-400">
+                {settledString}
+            </div>
         </button>
     );
 }
--- bill-settler-app/src/components/home/HomeContainer.tsx
+++ bill-settler-app/src/components/home/HomeContainer.tsx
@@ -12,10 +12,10 @@ export default function HomeContainer() {
                 <HomeButtons/>
             </div>
             <h1 class="text-3xl font-bold my-12">Recent Expenses</h1>
-            <ExpenseComponent expense={new Expense("name1", "owner1", 15)}/>
-            <ExpenseComponent expense={new Expense("name2", "owner2", 123.11)}/>
-            <ExpenseComponent expense={new Expense("name3", "owner1", .12)}/>
-            <ExpenseComponent expense={new Expense("name4", "owner4", 10.01)}/>
+            <ExpenseComponent expense={new Expense("name1", "owner1", 15, true)}/>
+            <ExpenseComponent expense={new Expense("name2", "owner2", 123.11, true)}/>
+            <ExpenseComponent expense={new Expense("name3", "owner1", .12, false)}/>
+            <ExpenseComponent expense={new Expense("name4", "owner4", 10.01, true)}/>
         </div>
     );
 }
